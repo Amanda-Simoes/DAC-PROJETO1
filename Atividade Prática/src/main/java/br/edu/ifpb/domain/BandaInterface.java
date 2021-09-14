fc316368f7e92d18f73faf9c1431061906e104be
Amanda Simoes <amanda.simoes1707@gmail.com>
2021-09-14 16:29:19 -0300

    Interfaces criadas

---
+++ Atividade Prática/src/main/java/br/edu/ifpb/domain/BandaInterface.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author mandy
+ */
+public interface BandaInterface extends Serializable{
+    
+    public List<Banda> listaBanda();
+    
+    public void addBanda (Banda banda);
+    
+    public void updateBanda (Banda banda);
+    
+    public void deleteBanda (Banda banda);
+    
+    public Banda searchBanda(String localDeOrigem);
+    
+}
---
+++ Atividade Prática/src/main/java/br/edu/ifpb/domain/IntegranteInterface.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifpb.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author mandy
+ */
+public interface IntegranteInterface extends Serializable{
+    
+    public List<Integrante> listaIntegrante();
+    
+    public void addIntegrante (Integrante integrante);
+    
+    public void updateIntegrante (Integrante integrante);
+    
+    public void deleteIntegrante (Integrante integrante);
+    
+    public void searchIntegrante (String cpf);
+    
+}
