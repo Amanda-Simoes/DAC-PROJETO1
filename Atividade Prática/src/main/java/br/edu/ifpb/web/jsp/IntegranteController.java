2131c5e1db8bfab0d1b343980499257042e1e0f6
Amanda Simoes <amanda.simoes1707@gmail.com>
2021-09-19 18:19:42 -0300

    delete

--- Atividade Prática/src/main/java/br/edu/ifpb/web/jsp/IntegranteController.java
+++ Atividade Prática/src/main/java/br/edu/ifpb/web/jsp/IntegranteController.java
@@ -25,7 +25,7 @@ import javax.inject.Named;
 public class IntegranteController implements Serializable{
     
     private String resultIntegrante = "";
-    private Integrante integrante = new Integrante("", LocalDate.of(0000,00,00), new CPF(""));
+    private Integrante integrante = new Integrante("", LocalDate.of(1,1,1), new CPF(""));
     private IntegranteInterface integrantes;
     
     
@@ -70,7 +70,7 @@ public class IntegranteController implements Serializable{
         } else{
             this.integrantes.addIntegrante(this.integrante);
         }
-        this.integrante = new Integrante("", LocalDate.of(0000,00,00), new CPF(""));
+        this.integrante = new Integrante("", LocalDate.of(1,1,1), new CPF(""));
         return "/integrantes/list?faces-redirect=true";
     }
     
